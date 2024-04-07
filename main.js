e1f39a5d266130998aa51c81e84f5a4e661f5b88
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-07 16:46:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let currencyData = {
+    USD: 1,
+    EUR: 0.92,
+    GBP: 0.79,
+    JPY: 151.61,
+    AUD: 1.52,
+    CAD: 1.36,
+    CHF: 0.90,
+    CNY: 7.23,
+    INR: 83.30,
+    PKR: 277.54,
+};
+const curency = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: "Select the currency you want to convert from:",
+        choices: ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        type: "list",
+        message: "Select the currency you want to convert to:",
+        choices: ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "INR", "PKR"]
+    },
+    { name: "amount",
+        type: "number",
+        message: "Enter the amount you want to convert: "
+    }
+]);
+let fromCurrency = currencyData[curency.from];
+let toCurrency = currencyData[curency.to];
+let amount1 = curency.amount;
+let baseCurency = amount1 / fromCurrency;
+let currencyConvertor = baseCurency * toCurrency;
+console.log(currencyConvertor);
